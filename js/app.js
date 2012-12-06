94f2a7a62ecff562a440af70cd92a81ba63acd10
Paul Allies <paul.allies@gmail.com>
2012-12-06 13:52:50 +0200

    changed the rest service to mwebs email service

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 function list(data){
 	$("#emaillist").empty();
 	var output = '<ul data-role="listview">';
-	$.each(data, function(key, value){
+	$.each(data.Messages, function(key, value){
 		output += '<li><a href="#msg" data-transition="slide">';
-		output += '<h2>' + value.headers.from[0] + '</h2>';
-		output += '<p>' + value.headers.subject[0] + '</p>';
-		output += '<p class="ui-li-aside">' + value.date + '</p>';
+		output += '<h2>' + value.From.DisplayName + '</h2>';
+		output += '<p>' + value.Subject + '</p>';
+		output += '<p class="ui-li-aside">' + value.Date + '</p>';
 		output += '</a></li>';
 	});
 	output += '</ul>';
 	$("#emaillist").html(output).trigger('create');
 	$.mobile.hidePageLoadingMsg();
 }
+
