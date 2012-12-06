2da908594ae449a70de31ea11efd82cb03afe40c
Paul Allies <paul.allies@gmail.com>
2012-12-06 11:11:54 +0200

    added phonegap.js file for debugging

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ function list(data){
 	$("#emaillist").empty();
 	var output = '<ul data-role="listview">';
 	$.each(data, function(key, value){
-		output += '<li><a href="#msg">';
+		output += '<li><a href="#msg" data-transition="slide">';
 		output += '<h2>' + value.headers.from[0] + '</h2>';
 		output += '<p>' + value.headers.subject[0] + '</p>';
 		output += '<p class="ui-li-aside">' + value.date + '</p>';
