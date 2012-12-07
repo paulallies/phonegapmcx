62c8147049af1f9fda2653bc63b8e86a0296abe8
Paul Allies <paul.allies@gmail.com>
2012-12-07 14:35:45 +0200

    updated message page with information

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 function list(data){
 	$("#emaillist").empty();
-	var output = '<ul data-role="listview">';
+	var output = '<ul data-role="listview" data-filter="true">';
 	 $.each(data, function(key, value){
-	 	output += '<li><a href="#msg" data-transition="slide" onclick="showEmail('+value.uid+')">';
-	 	output += '<h2>' + value.headers.from[0] + '</h2>';
-	 	output += '<p>' + value.headers.subject[0] + '</p>';
-	 	output += '<p class="ui-li-aside">' + value.date.split(' ')[0] + '</p>';
+	 	output += '<li uid="'+value.uid+'" ><a href="#msg" data-transition="slide" onclick="showEmail('+value.uid+')">';
+	 	output += '<h2 class="from">' + value.headers.from[0] + '</h2>';
+	 	output += '<p class="subject">' + value.headers.subject[0] + '</p>';
+	 	output += '<p class="ui-li-aside date">' + value.date.split(' ')[0] + '</p>';
 	 	output += '</a></li>';
 	 });
 	output += '</ul>';
@@ -13,10 +13,16 @@ function list(data){
 	$.mobile.hidePageLoadingMsg();
 }
 
-function showEmail(id){
-	$("#emailContent").html("");
-	$.getJSON("http://nodeimap.apphb.com/msg/"+id, function(data){
-		$("#emailContent").html(data.headers.received);
+function showEmail(uid){
+	var from  = $("li[uid="+uid+"] h2.from").html();
+	var subject  = $("li[uid="+uid+"] p.subject").html();
+	var date  = $("li[uid="+uid+"] p.date").html();
+	$("#msg .from").html(from);
+	$("#msg .subject").html(subject);
+	$("#msg .date").html(date);
+	$("#body").html("Loading...");
+	$.getJSON("http://nodeimap.apphb.com/msg/"+uid, function(data){
+		$("#body").html(data.headers.received);
 	})
 }
 
